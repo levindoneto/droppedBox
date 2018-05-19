7eb267bdf6bb8f9b22eadca81f93e334b02f3666
Levindo Gabriel Taschetto Neto <levindogtn@gmail.com>
2018-05-19 18:56:05 -0300

    Add events for watching the users' folders

--- utils/fileSystem/src/file.cpp
+++ utils/fileSystem/src/file.cpp
@@ -1,34 +1,11 @@
-#include <string>
 #include "../headers/file.hpp"
+#include "../../headers/dropboxUtils.hpp"
+#include <sys/types.h>
+#include <sys/stat.h>
 
 using namespace std;
 
-File::File(string fileName, string whenAccessed, string whenChanged, string whenCreated) {
-  // TODO: Handle folder for getting its path as a string if needed here
-  // Warning: redefinition because of ClientCommunication
-  this->fileName = fileName;
-  this->whenAccessed = whenAccessed;
-  this->whenChanged = whenChanged;
-  this->whenCreated = whenCreated;
-}
-
-string File::getFileName() {
-  return this->fileName;
-}
-
-string File::getWhenChanged() {
-  return this->whenAccessed;
-}
-
-string File::getWhenAccessed() {
-  return this->whenChanged;
-}
-
-string File::getWhenCreated() {
-  return this->whenCreated;
-}
-
-bool File::fileExists (char *filename) {
+bool fileExists (char *filename) {
   struct stat buffer;
   return (stat (filename, &buffer) == EQUAL);
 }
