a961d5c0eb9bbe44bc81be3fbaa236b55f06f0b6
Levindo Gabriel Taschetto Neto <levindogtn@gmail.com>
2018-06-14 01:17:49 -0300

    Add starting election and server info structure

--- server/headers/multiserver.hpp
+++ server/headers/multiserver.hpp
@@ -2,10 +2,9 @@
 #define MULTISERVER_HPP
 
 #include <list>
-#include<iostream>
-#include<vector>
-
-#include<mutex>
+#include <iostream>
+#include <string>
+#include <mutex>
 
 #include <unistd.h>
 #include <stdio.h>
@@ -27,17 +26,30 @@
 #include "../../utils/headers/udpUtils.hpp"
 #include "../../utils/headers/process.hpp"
 
+using namespace std;
+
+typedef struct serverInfo {
+  string ip;
+  int port;
+  int pid;
+} SERVER_INFO;
+
 class MultiServer {
   public:
     MultiServer() {};
     ~MultiServer();
+    void serverInfoInit();
+    SERVER_INFO getServerInfo();
     void startElectionProcesses();
-    void setProcessElected (int procElected);
-    void broadcastElected ();
-    list<int> servers; // pid_t
+    void setProcessElected(int procElected);
+    void broadcastElected();
+    list<int> servers;
 
   private:
     int procElected;
+    SERVER_INFO serverInfo;
+    vector<SERVER_INFO> listActiveProcesses;
+    vector<SERVER_INFO> inElectionProcesses;
 };
 
 #endif
--- server/src/multiserver.cpp
+++ server/src/multiserver.cpp
@@ -4,14 +4,35 @@
 // Dynamic initialization for mutex with specified attributes
 pthread_mutex_t startElection_mutex = PTHREAD_MUTEX_INITIALIZER;
 
+// Initialize the structure with the process' information (ip, port, pid of the
+// process running it). It's called in the constructor of this class.
+void MultiServer::serverInfoInit() {
+  string ipTest = "localhost";
+  this->serverInfo.ip = ipTest;
+  this->serverInfo.port = 3000;
+  this->serverInfo.pid = (int) getpid();
+}
+
+// Return the information about the process running the server
+SERVER_INFO MultiServer::getServerInfo() {
+  return this->serverInfo;
+}
+
 // Start an election using the bully algorithm for all active processes
 void MultiServer::startElectionProcesses() {
   pthread_mutex_lock(&startElection_mutex);
-  printf("in - Start election%s\n");
+  printf("Start election\n");
+  this->inElectionProcesses.push_back(this->serverInfo);
+  for (int i = 0; i < this->listActiveProcesses.size(); i++) {
+    if (this->serverInfo.pid < this->listActiveProcesses.at(i).pid) {
+      this->inElectionProcesses.push_back(this->listActiveProcesses.at(i));
+    }
+  }
+  printf("End election\n");
   pthread_mutex_unlock(&startElection_mutex);
-  printf("out%s\n");
 }
 
+// TODO: Change type, call broadcast afterward
 void MultiServer::setProcessElected (int procElected) {
   this->procElected = procElected;
 }
