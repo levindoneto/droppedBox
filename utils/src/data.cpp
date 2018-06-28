6f59dd987e656b6184a485c30fd5b74adcd4c802
Levindo Gabriel Taschetto Neto <levindogtn@gmail.com>
2018-06-28 09:57:13 -0300

    Add sync messages and stringfy method

--- utils/src/data.cpp
+++ utils/src/data.cpp
@@ -22,52 +22,68 @@ const string Data::T_DELETE = "DELETE";
 const string Data::T_SERVER_ELECTION = "SERVER_ELECTION";
 const string Data::T_WORKING = "WORKING";
 const string Data::T_COORDINATOR = "COORDINATOR";
+const string Data::T_NEW_USER = "NEW_USER";
+const string Data::T_IP = "IP";
+const string Data::T_BACKUP = "BACKUP";
+const string Data::T_OK = "OK";
 
 bool Data::pedindo() {
-    return (
-      type == Data::T_LS
-      || type == Data::T_DOWNLOAD
-      || type == Data::T_UPLOAD
-      || type == Data::T_BYE
-    );
+  return (
+    type == Data::T_LS
+    || type == Data::T_DOWNLOAD
+    || type == Data::T_UPLOAD
+    || type == Data::T_BYE
+    || type == Data::T_NEW_USER
+  );
 }
 
 Data::Data(string session, int sequence, string type, string content = "") {
-    this->session = session;
-    this->sequence = sequence;
-    this->type = type;
-    this->content = content;
+  this->session = session;
+  this->sequence = sequence;
+  this->type = type;
+  this->content = content;
 }
 
 string Data::to_string() {
-    string msg = "";
-    msg += session;
-    msg += Data::MSG_SEPARATOR;
-    msg += std::to_string(sequence);
-    msg += Data::MSG_SEPARATOR;
-    msg += type;
-    msg += Data::MSG_SEPARATOR;
-    msg += content;
-    msg += Data::MSG_SEPARATOR;
-    return msg;
+  string msgData = "";
+  msgData += session;
+  msgData += Data::MSG_SEPARATOR;
+  msgData += std::to_string(sequence);
+  msgData += Data::MSG_SEPARATOR;
+  msgData += type;
+  msgData += Data::MSG_SEPARATOR;
+  msgData += content;
+  msgData += Data::MSG_SEPARATOR;
+  return msgData;
 }
 
-Data Data::parse(string msg) {
-    // Find position of separators
-    int session_sep = msg.find('|');
-    int sequence_sep = msg.find('|', session_sep + 1);
-    int type_sep = msg.find('|', sequence_sep + 1);
-    int content_sep = msg.find_last_of('|');
+Data Data::parse(string msgData) {
+  // Find position of separators
+  int session_sep = msgData.find('|');
+  int sequence_sep = msgData.find('|', session_sep + 1);
+  int type_sep = msgData.find('|', sequence_sep + 1);
+  int content_sep = msgData.find_last_of('|');
+  int session_len = session_sep;
+  int sequence_len = sequence_sep - session_sep - 1;
+  int type_len = type_sep - sequence_sep - 1;
+  int content_len = content_sep - type_sep - 1;
+  string session = msgData.substr(INIT, session_len);
+  string sequence = msgData.substr(session_sep + 1, sequence_len);
+  string type = msgData.substr(sequence_sep + 1, type_len);
+  string content = msgData.substr(type_sep + 1, content_len);
 
-    int session_len = session_sep;
-    int sequence_len = sequence_sep - session_sep - 1;
-    int type_len = type_sep - sequence_sep - 1;
-    int content_len = content_sep - type_sep - 1;
+  return Data(session, stoi(sequence), type, content);
+}
 
-    string session = msg.substr(INIT, session_len);
-    string sequence = msg.substr(session_sep + 1, sequence_len);
-    string type = msg.substr(sequence_sep + 1, type_len);
-    string content = msg.substr(type_sep + 1, content_len);
-
-    return Data(session, stoi(sequence), type, content);
+string Data::stringify() {
+  string msgData = "";
+  msgData += session;
+  msgData += Data::MSG_SEPARATOR;
+  msgData += to_string(sequence);
+  msgData += Data::MSG_SEPARATOR;
+  msgData += to_string(static_cast<int>(type));
+  msgData += Data::MSG_SEPARATOR;
+  msgData += content;
+  msgData += Data::MSG_SEPARATOR;
+  return msgData;
 }
