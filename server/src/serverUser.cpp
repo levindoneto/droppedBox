c2d9ab7ae26a42d8714656d7d1221f5a74db04c8
Levindo Gabriel Taschetto Neto <levindogtn@gmail.com>
2018-06-28 17:04:55 -0300

    Modify server user to handle backups, Fix getting hostname on process communication

--- server/src/serverUser.cpp
+++ server/src/serverUser.cpp
@@ -4,7 +4,7 @@ DropboxServer#include "../headers/serverUser.hpp"
 #include "../../utils/headers/dropboxUtils.h"
 #include "../../utils/fileSystem/headers/folder.hpp"
 
-ServerUser::ServerUser(DropboxServer* server, Process *process) {
+ServerUser::ServerUser(DropboxServer *server, Process *process) {
   usingActive = true;
   this->server = server;
   this->process = process;
@@ -34,14 +34,6 @@ void ServerUser::mainloop() {
     Data request = process->receive_request();
     if (request.type == Data::T_UPLOAD) {
       receive_upload(request.content);
-      for (pair<string, UDPUtils> backup : DropboxServer::backupServers) {
-        Data msg = Data(NULL,
-          NULL,
-          request.type,
-          username + SLASH + request.content
-        );
-        backup.second.send(msg.stringify());
-      }
     }
     else if (request.type == Data::T_DOWNLOAD) {
       send_download(request.content);
@@ -49,58 +41,58 @@ void ServerUser::mainloop() {
     else if (request.type == Data::T_LS) {
       list_server();
     }
-    else if (request.type == Data::T_NEW_USER) {
-      for (pair<string, UDPUtils> backup : Server::backupServers) {
-        Data msg = Data(NULL, NULL, Data::T_IP, request.content);
-        backup.second.send(msg.stringify());
-      }
-    }
-    else if (request.type == Data::T_BYE) {
-      break;
-    }
+    else if (request.type == Data::T_BYE) break;
+  }
+  for (Process *backup : server->backupServers) {
+    backup->send(Data::T_CLIENT_DISCONNECT, process->ip);
   }
   cout << "User " << username << " logged out." << endl;
   usingActive = false;
 }
 
-void ServerUser::receive_upload(string filename, Process* process) {
+void ServerUser::receive_upload(string nameOfTheFile, Process *process) {
   if (!process) {
     process = this->process;
   }
-  string filepath = username + '/' + filename;
-  if (allowSending(filename)) {
+  string filepath = username + '/' + nameOfTheFile;
+  if (allowSending(nameOfTheFile)) {
     process->send(Data::T_OK);
-    cout << username << " is uploading " << filename << "..." << endl;
+    cout << "The user " << username << " is uploading " << nameOfTheFile << endl;
     process->receive_file(filepath);
-    unlock_file(filename);
-    cout << username << " uploaded " << filename << endl;
+    for (Process *backup : server->backupServers) {
+      backup->send(Data::T_UPLOAD, filepath);
+      backup->send_file(filepath); // TODO: Add to process.cpp
+    }
+    unlock_file(nameOfTheFile);
+    cout << "The user " << username << " successfully uploaded the file"
+      << nameOfTheFile << endl;
   }
   else {
-    cout << "Error: File " << filename << " currently syncing" << endl;
+    cout << "Error: File " << nameOfTheFile << " currently syncing" << endl;
     process->send(Data::T_ERROR, "File currently syncing");
     return;
   }
 }
 
-void ServerUser::send_download(string filename, Process* process) {
+void ServerUser::send_download(string nameOfTheFile, Process *process) {
   if (!process) {
     process = this->process;
   }
-  string filepath = username + '/' + filename;
+  string filepath = username + '/' + nameOfTheFile;
 
   if (!fileInFolder(filepath)) {
-    cout << "Error opening file " << filename << " at " << username << endl;
+    cout << "Error opening file " << nameOfTheFile << " at " << username << endl;
     process->send(Data::T_ERROR, "File not found");
     return;
   }
-  if (allowSending(filename)) {
+  if (allowSending(nameOfTheFile)) {
     process->send(Data::T_OK);
-    cout << username << " is downloading " << filename << "..." << endl;
+    cout << username << " is downloading " << nameOfTheFile << "..." << endl;
     process->send_file(filepath);
-    cout << username << " downloaded " << filename << endl;
-    unlock_file(filename);
+    cout << username << " downloaded " << nameOfTheFile << endl;
+    unlock_file(nameOfTheFile);
   } else {
-      cout << "Error: File " << filename << " currently syncing" << endl;
+      cout << "Error: File " << nameOfTheFile << " currently syncing" << endl;
       process->send(Data::T_ERROR, "File currently syncing");
   }
 }
