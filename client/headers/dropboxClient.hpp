300af364826bc05da087b794708d73b55908d56d
Levindo Gabriel Taschetto Neto <levindogtn@gmail.com>
2018-06-28 20:13:51 -0300

    Add getting new primary

--- client/headers/dropboxClient.hpp
+++ client/headers/dropboxClient.hpp
@@ -2,18 +2,23 @@
 #define DROPBOXCLIENT_H
 
 #include "../../utils/headers/dropboxUtils.h"
+#include "../../utils/headers/parUtils.hpp"
 #include "../../utils/headers/process.hpp"
 
-class DropboxClient {
+class DropboxClient : public Thread {
   private:
     // Where the commands are in (link with the server via UDP functions)
-    int run();
+    int mainloop();
     string userId;
   public:
+    DropboxClient(string username, string hostname, int port);
     ~DropboxClient();
+    void *run();
+    int port;
     string folderOfTheUser;
     Process *process;
-    void newProcessCommunication(string userId, string host, int port);
+    string username;
+    string hostname;
 };
 
 #endif
